e4cdd7a93576c3f9b7869faff2d9997420de882d
Pierre Klink <dev@klinks.info>
2013-12-15 15:46:01 +0100

    added some grunt-plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,54 @@ module.exports = function(grunt) {
                 dest: 'build/<%= pkg.name %>.min.js'
             }
         },
+        cssmin: {
+            minify: {
+                src:  'css/*.css',
+                dest: 'build/<%= pkg.name %>.min.css'
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            js: {
+                src: [
+                    'bower_components/jquery/jquery.min.js',
+                    'bower_components/semantic-ui/build/packaged/javascript/semantic.min.js',
+                    'bower_components/handlebars/handlebars.js',
+                    'bower_components/ember/ember.js',
+                    'bower_components/ember-data/ember-data.js',
+                    'bower_components/ember-localstorage-adapter/localstorage_adapter.js'
+                ],
+                dest: 'build/vendor.js'
+            }
+        },
+        copy: {
+            semantic: {
+                files: [{
+                    expand: true,
+                    src: 'bower_components/semantic-ui/build/packaged/fonts/*',
+                    dest: 'build/semantic-ui/fonts/',
+                    filter: 'isFile',
+                    flatten: true
+                }, {
+                    expand: true,
+                    src: 'bower_components/semantic-ui/build/packaged/css/semantic.min.css',
+                    dest: 'build/semantic-ui/css',
+                    filter: 'isFile',
+                    flatten: true
+                }]
+            }
+        }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['copy', 'concat', 'uglify', 'cssmin']);
 
 };
